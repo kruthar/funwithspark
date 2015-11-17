529dc7946de90d3fd34664c2f4a53aaa750ba7aa
Andy Kruth <kruthar@gmail.com>
2015-11-17 09:12:17 -0600

    weather example reorg

--- src/main/scala/com/kruth/scala/examples/weather/Weather.scala
+++ src/main/scala/com/kruth/scala/examples/weather/Weather.scala
@@ -1,4 +1,4 @@
-package com.kruth.scala
+package com.kruth.scala.examples.weather
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
@@ -8,94 +8,18 @@ import scala.util.matching.Regex
 /**
   * Created by kruthar on 11/16/15.
   */
-object WeatherFunctions {
-  val daysInMonths = List(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-
-  /**
-    * Converts a Celsius temperature to Fharenheit
-    * @param temp
-    * @return temperature in Fharenheit
-    */
-  def CelsiusToFahrenheit(temp: Double): Double = {
-    return temp * 9 / 5 + 32
-  }
-
-  /**
-    * Pad the front of a given string with 0
-    * @param day
-    * @param len
-    * @return padded String
-    */
-  def padWithZero(day: String, len: Int): String = {
-    if (day.length() < len) {
-      var result = day
-      while (result.length < len) {
-        result = "0" + result
-      }
-      return result
-    }
-    return day
-  }
-
-  /**
-    * Formats date string in format yyyyMMdd to format yyyy-MM-dd
-    * @param dateString
-    * @return formatted date string
-    */
-  def formatDateString(dateString: String): String = {
-    return dateString.substring(0, 4) + "-" + dateString.substring(4, 6) + "-" + dateString.substring(6, 8)
-  }
-
-  /**
-    * Is the given year a leap year?
-    * @param year
-    * @return true if given year is a leap year, false otherwise
-    */
-  def isLeapYear(year: Int): Boolean = {
-    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-      return true
-    }
-    return false
-  }
-
-  /**
-    * Given a date calculate the day out of 365/366 of the year
-    * @param year
-    * @param month
-    * @param day
-    * @return the day of the year number
-    */
-  def getDayOfYear(year: Int, month: Int, day: Int): Int = {
-    var doy = 0
-    var counter = 0
-    while (counter < month - 1) {
-      doy += daysInMonths(counter)
-      counter += 1
-    }
-    doy += day
-
-    if (month > 2 && isLeapYear(year)) {
-      doy += 1
-    }
-
-    return doy
-  }
-}
-
 object Weather {
   def main(args: Array[String]): Unit = {
     val monthlyWeatherSplitRegex = new Regex("^(.{11})(.{4})(.{2})(.{4})(.{8})(.{8})(.{8})(.{8})(.{8})" +
       "(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})" +
       "(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})$")
     val dailyValueSplitRegex = new Regex("^(.{5})(.)(.)(.)$")
-    val dateFormat = new java.text.SimpleDateFormat("yyyyMMdd")
 
-    val conf = new SparkConf()
-      .setAppName("Weather")
+    val conf = new SparkConf().setAppName("Weather")
     val sc = new SparkContext(conf)
 
     /**
-      * Grab a local weather text file with lines of the form of:
+      * Grab a local weather text file(s) with lines of the form of:
       * AE000041196194403TMAX-9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   \
       * -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999     380  I  346  I  319  I  302  I  296  I  \
       * 308  I  291  I  296  I  296  I  335  I  396  I  313  I
---
+++ src/main/scala/com/kruth/scala/examples/weather/WeatherFunctions.scala
@@ -0,0 +1,78 @@
+package com.kruth.scala.examples.weather
+
+/**
+  * Created by kruthar on 11/16/15.
+  */
+object WeatherFunctions {
+  val daysInMonths = List(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+  /**
+    * Converts a Celsius temperature to Fharenheit
+    * @param temp
+    * @return temperature in Fharenheit
+    */
+  def CelsiusToFahrenheit(temp: Double): Double = {
+    return temp * 9 / 5 + 32
+  }
+
+  /**
+    * Pad the front of a given string with 0
+    * @param day
+    * @param len
+    * @return padded String
+    */
+  def padWithZero(day: String, len: Int): String = {
+    if (day.length() < len) {
+      var result = day
+      while (result.length < len) {
+        result = "0" + result
+      }
+      return result
+    }
+    return day
+  }
+
+  /**
+    * Formats date string in format yyyyMMdd to format yyyy-MM-dd
+    * @param dateString
+    * @return formatted date string
+    */
+  def formatDateString(dateString: String): String = {
+    return dateString.substring(0, 4) + "-" + dateString.substring(4, 6) + "-" + dateString.substring(6, 8)
+  }
+
+  /**
+    * Is the given year a leap year?
+    * @param year
+    * @return true if given year is a leap year, false otherwise
+    */
+  def isLeapYear(year: Int): Boolean = {
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+      return true
+    }
+    return false
+  }
+
+  /**
+    * Given a date calculate the day out of 365/366 of the year
+    * @param year
+    * @param month
+    * @param day
+    * @return the day of the year number
+    */
+  def getDayOfYear(year: Int, month: Int, day: Int): Int = {
+    var doy = 0
+    var counter = 0
+    while (counter < month - 1) {
+      doy += daysInMonths(counter)
+      counter += 1
+    }
+    doy += day
+
+    if (month > 2 && isLeapYear(year)) {
+      doy += 1
+    }
+
+    return doy
+  }
+}
